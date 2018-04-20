3bb801e445ca349fb40b2ae00f29942cc7cc7517
Vikram Jakhar <vikram.jakhar@tothenew.com>
2018-04-20 16:04:26 +0530

    usage added

---
+++ cpu.go
@@ -0,0 +1,42 @@
+package main
+
+import ()
+
+type CPUStats struct {
+	ps             PS
+	PerCPU         bool `toml:"percpu"`
+	TotalCPU       bool `toml:"totalcpu"`
+	CollectCPUTime bool `toml:"collect_cpu_time"`
+	ReportActive   bool `toml:"report_active"`
+}
+
+func NewCPUStats(ps PS) *CPUStats {
+	return &CPUStats{
+		ps:             ps,
+		CollectCPUTime: true,
+		ReportActive:   true,
+	}
+}
+
+func (_ *CPUStats) Description() string {
+	return "Read metrics about cpu usage"
+}
+
+var sampleConfig = `
+  ## Whether to report per-cpu stats or not
+  percpu = true
+  ## Whether to report total system cpu stats or not
+  totalcpu = true
+  ## If true, collect raw CPU time metrics.
+  collect_cpu_time = false
+  ## If true, compute and report the sum of all non-idle CPU states.
+  report_active = false
+`
+
+func (_ *CPUStats) SampleConfig() string {
+	return sampleConfig
+}
+
+func (s *CPUStats) Gather() error {
+	return nil
+}
--- input.go
+++ input.go
@@ -12,3 +12,15 @@ type Input interface {
 	Gather() error
 }
 
+type Output interface {
+	// Connect to the Output
+	Connect() error
+	// Close any connections to the Output
+	Close() error
+	// Description returns a one-sentence description on the Output
+	Description() string
+	// SampleConfig returns the default configuration of the Output
+	SampleConfig() string
+	// Write takes in group of points to be written to the Output
+	Write() error
+}
