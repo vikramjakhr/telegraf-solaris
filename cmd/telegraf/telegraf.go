6e0b957673948c77c5bd63f6d1210de63001bd6a
Vikram Jakhar <vikram.jakhar@tothenew.com>
2018-04-20 14:54:53 +0530

    first commit

---
+++ cmd/telegraf/telegraf.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	_ "net/http/pprof" // Comment this line to disable pprof endpoint.
+	"os"
+)
+
+var fDebug = flag.Bool("debug", false,
+	"turn on debug logging")
+var pprofAddr = flag.String("pprof-addr", "",
+	"pprof address to listen on, not activate pprof if empty")
+var fQuiet = flag.Bool("quiet", false,
+	"run in quiet mode")
+var fTest = flag.Bool("test", false, "gather metrics, print them out, and exit")
+var fConfig = flag.String("config", "", "configuration file to load")
+var fConfigDirectory = flag.String("config-directory", "",
+	"directory containing additional *.conf files")
+var fVersion = flag.Bool("version", false, "display the version")
+var fSampleConfig = flag.Bool("sample-config", false,
+	"print out full sample configuration")
+var fPidfile = flag.String("pidfile", "", "file to write our pid to")
+var fInputFilters = flag.String("input-filter", "",
+	"filter the inputs to enable, separator is :")
+var fInputList = flag.Bool("input-list", false,
+	"print available input plugins.")
+var fOutputFilters = flag.String("output-filter", "",
+	"filter the outputs to enable, separator is :")
+var fOutputList = flag.Bool("output-list", false,
+	"print available output plugins.")
+var fAggregatorFilters = flag.String("aggregator-filter", "",
+	"filter the aggregators to enable, separator is :")
+var fProcessorFilters = flag.String("processor-filter", "",
+	"filter the processors to enable, separator is :")
+var fUsage = flag.String("usage", "",
+	"print usage for a plugin, ie, 'telegraf --usage mysql'")
+var fService = flag.String("service", "",
+	"operate on the service")
+
+var (
+	nextVersion = "1.5.0"
+	version     string
+	commit      string
+	branch      string
+)
+
+func init() {
+	// If commit or branch are not set, make that clear.
+	if commit == "" {
+		commit = "unknown"
+	}
+	if branch == "" {
+		branch = "unknown"
+	}
+}
+
+const usage = `Telegraf, The plugin-driven server agent for collecting and reporting metrics.
+
+Usage:
+
+  telegraf [commands|flags]
+
+The commands & flags are:
+
+  config              print out full sample configuration to stdout
+  version             print the version to stdout
+
+  --config <file>     configuration file to load
+  --test              gather metrics once, print them to stdout, and exit
+  --config-directory  directory containing additional *.conf files
+  --input-filter      filter the input plugins to enable, separator is :
+  --output-filter     filter the output plugins to enable, separator is :
+  --usage             print usage for a plugin, ie, 'telegraf --usage mysql'
+  --debug             print metrics as they're generated to stdout
+  --pprof-addr        pprof address to listen on, format: localhost:6060 or :6060
+  --quiet             run in quiet mode
+
+Examples:
+
+  # generate a telegraf config file:
+  telegraf config > telegraf.conf
+
+  # generate config with only cpu input & influxdb output plugins defined
+  telegraf --input-filter cpu --output-filter influxdb config
+
+  # run a single telegraf collection, outputing metrics to stdout
+  telegraf --config telegraf.conf --test
+
+  # run telegraf with all plugins defined in config file
+  telegraf --config telegraf.conf
+
+  # run telegraf, enabling the cpu & memory input, and influxdb output plugins
+  telegraf --config telegraf.conf --input-filter cpu:mem --output-filter influxdb
+
+  # run telegraf with pprof
+  telegraf --config telegraf.conf --pprof-addr localhost:6060
+`
+
+func usageExit(rc int) {
+	fmt.Println(usage)
+	os.Exit(rc)
+}
+
+func displayVersion() string {
+	if version == "" {
+		return fmt.Sprintf("v%s~%s", nextVersion, commit)
+	}
+	return "v" + version
+}
+
+func main() {
+	fmt.Println("Starting....")
+	flag.Usage = func() { usageExit(0) }
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) > 0 {
+		switch args[0] {
+		case "version":
+			fmt.Printf("Telegraf %s (git: %s %s)\n", displayVersion(), branch, commit)
+			return
+		}
+	}
+	fmt.Println("Done")
+}
